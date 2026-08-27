6509f0c8d2a751e398de7ebcc1920267185e607f
MiguelDS021 <ramoss.dev@gmail.com>
2026-08-27 20:53:26 -0300

    adiciona busca de produtos

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+id = 0
+categorias = ["Camisetas", "Calças", "Calçados", "Acessórios"]
+camiseta = ["P", "M", "G", "GG"]
+calça = [38, 40, 42, 44]
+calçados = [38, 39, 40, 42]
 
-def Cadastrar_Produto(valor):
+def Cadastrar_Produtos(id):
     try:    
         while True:
             nome = input("Digite o nome do produto: ").strip()
@@ -9,112 +14,59 @@ def Cadastrar_Produto(valor):
                 break  
 
         while True:
-            categoria_escolhida = int(input("Digite a categoria: [1] Camiseta | [2] Calça | [3] Calçados | [4] Acessórios"))
-            categoria = ""
+            cat_escolha = int(input("Digite a categoria: [1] Camiseta | [2] Calça | [3] Calçados | [4] Acessórios"))
+            
         
-            if categoria_escolhida != 1 and categoria_escolhida != 2 and categoria_escolhida != 3 and categoria_escolhida != 4:
+            if cat_escolha < 1 or cat_escolha > 4:
                 print("Erro: Digite uma opção válida.")
         
             else:   
-        
-                if categoria_escolhida == 1:
-                    categoria = "Camiseta"
-                    break
-
-                elif categoria_escolhida == 2:
-                    categoria = "Calça"
-                    break
-
-                elif categoria_escolhida == 3:
-                    categoria = "Calçados"
-                    break
-
-                elif categoria_escolhida == 4:
-                    categoria = "Acessórios"
-                    break
+                categoria = categorias[cat_escolha-1]
+                break
 
         while True:
-            if categoria == "Camiseta":
-                tamanho_escolhido = int(input("""Escolha o tamanho: 
+            if categoria == "Camisetas":
+                escolha = int(input("""Escolha o tamanho: 
                 [1] P
                 [2] M
                 [3] G
                 [4] GG """))
 
-                if tamanho_escolhido != 1 and tamanho_escolhido != 2 and tamanho_escolhido != 3 and tamanho_escolhido != 4:
+                if escolha < 1 or escolha > 4:
                     print("Erro: Escolha uma opção válida.")
 
                 else:
-                        if tamanho_escolhido == 1:
-                            tamanho = "P"
-                            break
+                    tamanho = camiseta[escolha-1]
+                    break
 
-                        elif tamanho_escolhido == 2:
-                            tamanho = "M"
-                            break
-
-                        elif tamanho_escolhido == 3:
-                            tamanho = "G"  
-                            break
-
-                        elif tamanho_escolhido == 4:
-                            tamanho = "GG"
-                            break
-
-            elif categoria == "Calça":
-                tamanho_escolhido = int(input("""Escolha o tamanho: 
+            elif categoria == "Calças":
+                escolha2 = int(input("""Escolha o tamanho: 
                 [1] 38
                 [2] 40
                 [3] 42
                 [4] 44 """))
                         
-                if tamanho_escolhido != 1 and tamanho_escolhido != 2 and tamanho_escolhido != 3 and tamanho_escolhido != 4:
-                        print("Erro: Escolha uma opção válida.")
+                if escolha2 < 1 or escolha2 > 4:
+                    print("Erro: Escolha uma opção válida.")
                         
                 else:
-                        if tamanho_escolhido == 1:
-                            tamanho = 38
-                            break
-
-                        elif tamanho_escolhido == 2:
-                            tamanho = 40
-                            break
-
-                        elif tamanho_escolhido == 3:
-                            tamanho = 42 
-                            break
-
-                        elif tamanho_escolhido == 4:
-                            tamanho = 44     
-                            break
+                    tamanho = calça[escolha2-1]
+                    break
 
             elif categoria == "Calçados":
-                tamanho_escolhido = int(input("""Escolha o tamanho: 
+                escolha3 = int(input("""Escolha o tamanho: 
                 [1] 38
                 [2] 39
                 [3] 40
                 [4] 41 """))
         
-                if tamanho_escolhido != 1 and tamanho_escolhido != 2 and tamanho_escolhido != 3 and tamanho_escolhido != 4:
+                if escolha3 < 1 or escolha3 > 4:
                     print("Erro: Escolha uma opção válida")
         
                 else:
-                    if tamanho_escolhido == 1:
-                        tamanho = "38"
-                        break
-        
-                    elif tamanho_escolhido == 2:
-                        tamanho = "39"
-                        break       
-        
-                    elif tamanho_escolhido == 3:
-                        tamanho = "40"  
-                        break
-        
-                    elif tamanho_escolhido == 4:
-                        tamanho = "41"        
-                        break
-        
+                    tamanho= calçados[escolha3-1]
+                    break
+
             elif categoria == "Acessórios":
                 tamanho = "Único"
                 break
@@ -137,16 +89,14 @@ def Cadastrar_Produto(valor):
         print("Erro: Em preço só é permitido números decimais e em estoque números inteiros.")    
     
     else:  
-        id = 0
-        id += valor
-        Adicionar_Produto(id, nome, categoria, tamanho, preço, estoque)    
+       
+        id += 1
+        Adicionar_Produtos(id, nome, categoria, tamanho, preço, estoque) 
+        return id
     
 
-
-
-
 Produtos = {}
-def Adicionar_Produto(id, nome, categoria, tamanho, preço, estoque):
+def Adicionar_Produtos(id, nome, categoria, tamanho, preço, estoque):
     
         Produtos[f"Produto{id}"] = {
             "ID": id,
@@ -158,23 +108,101 @@ def Adicionar_Produto(id, nome, categoria, tamanho, preço, estoque):
             
 }
 
+def Visualizar_Produtos():
+    if not Produtos:
+        print("O dicionário não possui produtos cadastrados")
+    else:
+        print(f"{'=' * 5} Produtos {'=' * 5}")
+        for chave, valor in Produtos.items():
+            print(f"""
+{chave}
+ID: {valor["ID"]}
+Nome: {valor["Nome"]}
+Categoria: {valor["Categoria"]}
+Tamanho: {valor["Tamanho"]}
+Preço: {valor["Preço"]}
+Estoque: {valor["Estoque"]}
+
+-------------""")
+
+def Buscar_Produtos():
+    while True:
+        alternativa = int(input("""Escolha uma alternativa:
+        [1] Buscar por id
+        [2] Buscar por nome
+        [3] Voltar"""))
+
+        if alternativa < 1 or alternativa > 3:
+            print("Erro: Escolha uma opção válida.")
+        else:
+            if alternativa == 1:
+                Buscar_Por_Id()
+
+            elif alternativa == 2:
+                Buscar_Por_Nome()
+
+            elif alternativa == 3:
+                break
+
+
+def Buscar_Por_Id():
+    busca = int(input("Digite o ID do produto: "))
+
+    encontrado = False
+    for chave, valor in Produtos.items():
+        if busca == valor["ID"]:
+            print(f"""{valor["ID"]}
+        {valor["Nome"]}
+        {valor["Categoria"]}
+        {valor["Tamanho"]}
+        {valor["Preço"]}
+        {valor["Estoque"]}""")
+            encontrado = True
+
+    if encontrado == False:
+        print("Este produto não existe") 
+                   
+def Buscar_Por_Nome():
+    procura = input("Escreva o nome do produto: ")
+
+    nome_encontrado = False
+    for chave, valor in Produtos.items():
+        if procura in valor["Nome"]:
+            print(f"""{valor["ID"]}
+        {valor["Nome"]}
+        {valor["Categoria"]}
+        {valor["Tamanho"]}
+        {valor["Preço"]}
+        {valor["Estoque"]}""")
+            nome_encontrado = True
+
+    if nome_encontrado == False:
+        print("Não existe produtos com este nome")
+
+
+
 
 while True:
-    while True:
+    
         opção = int(input("""Escolha uma opção:
-        [1] Adicionar produto
-        [2] Mostrar produto
-        [3] Sair """))
+        [1] Adicionar produtos
+        [2] Mostrar produtos
+        [3] Buscar produtos
+        [4] Sair """))
 
-        if opção != 1 and opção != 2  and opção != 3:
+        if opção < 1 or opção > 4:
             print("Erro: Escolha uma opção válida.")
         else:
             if opção == 1:
-                valor = 1
-                Cadastrar_Produto(valor)
+                
+                id = Cadastrar_Produtos(id)
 
             elif opção == 2:
-                print(Produtos)  
+                Visualizar_Produtos() 
 
-    if opção == 3:
-        break          
+            elif opção == 3:
+                Buscar_Produtos()
+
+            elif opção == 4:
+                break              
+            
